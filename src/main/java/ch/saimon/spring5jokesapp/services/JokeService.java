87d68992422a76d01b44a61aff1abce7129f0862
simonkuhn <simon.a94@hotmail.com>
2022-11-14 13:08:33 +0100

    show chuck norris jokes

---
+++ src/main/java/ch/saimon/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package ch.saimon.spring5jokesapp.services;
+
+public interface JokeService {
+
+    String tellJoke();
+}
---
+++ src/main/java/ch/saimon/spring5jokesapp/services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package ch.saimon.spring5jokesapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String tellJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
